aba8efc0058b15c80a45cd76cf85fd204c0c0480
AroofaAllauddin <160481304+AroofaAllauddin@users.noreply.github.com>
2024-03-01 09:30:27 -0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,15 @@
+//string//
+var Fullname = "Aroofa Allauddin";
+console.log("my name is " + Fullname);
+//number//
+var Myage = 19;
+console.log("My age is " + Myage);
+//Boolean//
+var check = true;
+console.log(check);
+//undefined//
+var abc = ["dua", " aroofa", "neha"];
+console.log(abc[4]);
+//null//
+var value = null;
+console.log(value);
---
+++ app.ts
@@ -0,0 +1,15 @@
+//string//
+let Fullname: string = "Aroofa Allauddin";
+console.log("my name is " + Fullname);
+//number//
+let Myage: number = 19;
+console.log("My age is " + Myage);
+//Boolean//
+let check: boolean = true;
+console.log(check)
+//undefined//
+let abc = ["dua", " aroofa", "neha"];
+console.log(abc[4])
+//null//
+let value: null = null;
+console.log(value);
